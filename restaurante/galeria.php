8ee6638247c17a4b61a5299d029361e096e2ce69
Mcp130 <51353647+Mcp130@users.noreply.github.com>
2021-08-01 02:37:19 -0600

    primera v

---
+++ restaurante/galeria.php
@@ -0,0 +1,44 @@
+<?php 
+include 'templates/cabecera.html'
+?>
+
+<div class="row border-bottom bd-lightGray m-3">
+    <div class="cell-md-12 d-flex flex-align-center">
+        <h2 class="dashboard-section-title text-center text-Center-md w-100">Galería</h3>
+    </div>
+</div>
+
+<div class="m-3">
+    <div class="row">
+        <div class="img-container drop-shadow cell-md-3 mt-3">
+            <img src="imgs/1.jpg">
+        </div>
+        <div class="img-container drop-shadow cell-md-3 mt-3">
+            <img src="imgs/2.jpg">
+        </div>
+        <div class="img-container drop-shadow cell-md-3 mt-3">
+            <img src="imgs/3.jpg">
+        </div>
+        <div class="img-container drop-shadow cell-md-3 mt-3">
+            <img src="imgs/4.jpg">
+        </div>
+        <div class="img-container drop-shadow cell-md-3 mt-3">
+            <img src="imgs/5.jpg">
+        </div>
+    </div
+></div>
+
+
+
+
+
+
+
+
+
+
+
+
+<?php 
+include 'templates/footer.html'
+?>
---
+++ restaurante/index.php
@@ -0,0 +1,64 @@
+<?php 
+include 'templates/cabecera.html'
+?>
+
+<div class="img-container">
+    <img src="imgs/ba.jpg">
+</div>
+
+<div class="row border-bottom bd-lightGray m-3">
+        <div class="cell-8 offset-2"> 
+            <div><p class="dashboard-section-title text-center text-Center-md w-100">
+                Restaurante Geros es un restaurante 100% familiar, el 
+                cual busca maneter un ambiente agradable para personas de todas las
+                edades, ofreciendote ricos platillos elaborados con mariscos, 
+                podras encontrar el tradicional "Chicarron de pescado" y nuestros exquisitos
+                "Langostinos al gusto", platillo que solo encontraras con nosotros.
+                <br><br>
+                Ademas contamos con distinitas actividades recreativas, tales como
+                ciclismo, renta de kayaks, renta de bicletas acuaticas, paseos en lancha 
+                y areas para acampar.
+                <br><br>
+                <b>"Atrevete a vivir la experiencia"</b>
+            </p></div>
+        </div>
+</div>
+
+<div class="m-3">
+    <div class="cell-md-12 d-flex flex-align-center">
+        <h3 class="dashboard-section-title text-center text-Center-md w-100"> Servicios </h3>    
+    </div>
+    <div class="row dashboard-section-title text-center text-Center-md w-100">
+        <div class="img-container drop-shadow cell-md-3 mt-3">
+            <img src="imgs/01.png">
+            <span class="title">
+                <b>Restaurante</b>
+            </span>
+        </div>
+        <div class="img-container drop-shadow cell-md-3 mt-3">
+            <img src="imgs/02.png">
+            <span class="title">
+                <b>Area de acampar</b>
+            </span>
+        </div>
+        <div class="img-container drop-shadow cell-md-3 mt-3">
+            <img src="imgs/03.png">
+            <span class="title">
+                <b>Paseos en lancha</b>
+            </span>
+        </div>
+        <div class="img-container drop-shadow cell-md-3 mt-3">
+            <img src="imgs/04.png">
+            <span class="title">
+                <b>Renta de equipo recreativo</b>
+            </span>
+        </div>
+    </div
+></div>
+
+
+
+
+<?php 
+include 'templates/footer.html'
+?>
